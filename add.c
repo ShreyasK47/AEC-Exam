63c802fa3931e9b148d43525a1048c541dcc6dc0
ShreyasK <shreyaskarevalli1@gmail.com>
2024-08-01 09:50:46 +0530

    Adding three numbers

--- add.c
+++ add.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 void main(){
-   int a=10,b=20;
-   int c=a+b;
-   printf("%d + %d = %d",a,b,c);
+   int a=10,b=20,d=20;
+   int c=a+b+d;
+   printf("%d + %d + %d= %d",a,b,d,c);
 }
