da032851379696a9270eee1c8f78216e2968b35f
ShreyasK <shreyaskarevalli1@gmail.com>
2024-08-01 09:51:49 +0530

    subtracting three numbers

--- sub.c
+++ sub.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 void main(){
-   int a=30,b=20;
-   int c=a-b;
-   printf("%d - %d = %d",a,b,c);
+   int a=30,b=20,d=2;
+   int c=a-b-d;
+   printf("%d - %d - %d = %d",a,b,d,c);
 }
